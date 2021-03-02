2ed79ca765cee45a4bdc35e7b0a25772fa23eeb7
Adel <79758154+3dol@users.noreply.github.com>
2021-03-02 20:55:47 +0300

    Initial commit

---
+++ js/set_footer_more_content.js
@@ -0,0 +1,15 @@
+window.onload = ()=> {
+    //setting footer
+    let windowHeight = window.innerHeight;
+    let bodyHeight = document.getElementById('body').scrollHeight;
+    let footer = document.getElementById('footer');
+    let newHeight = windowHeight - bodyHeight + 'px';
+    newHeight = newHeight.replace('-','');
+
+    if (windowHeight > 845){
+        footer.style.top = newHeight;
+        footer.style.visibility = 'visible';
+    }else {
+        footer.style.visibility = 'visible';
+    }
+}
